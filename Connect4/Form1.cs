b18c64d299eb4d47b013bbe1170e44b28d4bd622
Riku Ruotsalainen <gr189252@gradia.fi>
2020-02-19 12:05:40 +0200

    Add project files.

---
+++ Connect4/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        //Pelin ohjeet
+        private void Ohjeet_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Peliä pelataan 7 sarakkeen ja 6 rivin kokoisella pelilaudalla." +
+                " Molemmilla pelaajilla on käytössään 21 kiekkoa, jotka erottuvat toisistaan kahdella eri värillä." +
+                " Kiekkoja pudotetaan vuorotellen täyttämättömiin sarakkeisiin, jolloin kiekko varaa itselleen tyhjästä kohdasta sijansa." +
+                " Voittaakseen pelin täytyy pelaajan asettaa omia kiekkojaan niin, että muodostuu joko pystysuorassa, vaakasuorassa tai poikittain neljän kiekon suora." +
+                " Jos pelilauta täyttyy ilman voittajaa, syntyy tasapeli.");
+        }
+
+        private void database_Click(object sender, EventArgs e)
+        {
+            Pisteet f = new Pisteet();
+            this.Hide();
+            f.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            this.Hide();
+            f.ShowDialog();
+        }
+    }
+}
---
+++ Connect4/Pisteet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Connect4
+{
+    public partial class Pisteet : Form
+    {
+        public Pisteet()
+        {
+            InitializeComponent();
+        }
+
+        private void Pisteet_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = ConnectFourScore;User ID=LAPTOP-Q131GA2D\ruots;Trusted_Connection=yes;");
+            conn.Open();
+
+            SqlDataAdapter adapt;
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from Score", conn);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+
+            conn.Close();
+
+        }
+
+        private void mainMenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Menu f = new Menu();
+            this.Hide();
+            f.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
